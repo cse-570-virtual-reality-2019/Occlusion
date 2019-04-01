adcd4711c9328cf0d73e193e1055ceff505235b5
Harshit Panwar <harshit16043@iiitd.ac.in>
2019-04-01 21:46:27 +0530

    first commit

---
+++ assets/Scripts/image.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class image : MonoBehaviour
+{
+    public RawImage mRawImage;
+    public MultiSourceManager mMultiSCource;
+    // Update is called once per frame
+    void Update()
+    {
+        mRawImage.texture = mMultiSCource.GetColorTexture();
+        
+    }
+}
---
+++ assets/Scripts/measureDepth.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class measureDepth : MonoBehaviour
+{
+    private ushort[] mDepthData=null;
+    public MultiSourceManager mMultiSource;
+    public Material m1, m2;
+    public GameObject sphere;
+
+    private int arraySize;
+    private KinectSensor mSensor = null;
+    private void Awake()
+    {
+        mSensor = KinectSensor.GetDefault();
+    }
+    private void Update()
+    {
+        mDepthData = mMultiSource.GetDepthData();
+        if (check())
+        {
+           //Debug.Log(mDepthData[81795]);
+            sphere.GetComponent<Renderer>().material = m2;
+            //Debug.Log("Enter");
+        }
+        else
+        {
+            sphere.GetComponent<Renderer>().material = m1;
+            //Debug.Log(mDepthData[81795]);
+            //Debug.Log(mDepthData[arraySize / 2]);
+            //Debug.Log("Exit");
+        }
+    }
+    private bool check()
+    {
+        for (int i = 82634; i < 111818; i = i + 512)
+        {
+            for (int j = 0; j < 57; j++)
+            {
+                if (mDepthData[i + j] > 0 && mDepthData[i + j] < 900)
+                {
+                    Debug.Log(mDepthData[i + j]);
+                    return true;
+                }
+            }
+        }
+        //Debug.Log(mDepthData[82634]);
+        return false;
+    }
+}
