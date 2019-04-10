969eace366939a234d4d90eddd16a2f586316a17
Harshit Panwar <harshit16043@iiitd.ac.in>
2019-04-10 19:59:36 +0530

    sec

---
+++ assets/Scripts/Switch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Switch : MonoBehaviour
+{
+    public void switch1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void switch2()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
